b7b07269835578487a40375023e79c8b73ee691b
Artem Naumov <artem.naumov.dev@gmail.com>
2025-05-23 00:36:18 +0300

    fix: changed List to ListDbEntity in db entities

--- app/src/main/java/com/gwolf/nytbestsellers/data/database/NYTBestSellersDatabase.kt
+++ app/src/main/java/com/gwolf/nytbestsellers/data/database/NYTBestSellersDatabase.kt
@@ -4,12 +4,13 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.gwolf.nytbestsellers.data.database.dao.NYTBestSellersDao
 import com.gwolf.nytbestsellers.data.database.entity.BookDbEntity
+import com.gwolf.nytbestsellers.data.database.entity.ListDbEntity
 import com.gwolf.nytbestsellers.data.database.entity.ResultDbEntity
 import com.gwolf.nytbestsellers.util.DB_VERSION
 
 @Database(
     version = DB_VERSION,
-    entities = [ResultDbEntity::class, List::class, BookDbEntity::class]
+    entities = [ResultDbEntity::class, ListDbEntity::class, BookDbEntity::class]
 )
 abstract class NYTBestSellersDatabase : RoomDatabase() {
 
