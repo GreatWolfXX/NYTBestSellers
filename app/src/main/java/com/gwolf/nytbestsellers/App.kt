815539be5635f0e3f335183017c6ce659bedc152
GreatWolf20XX <artem.naumov.dev@gmail.com>
2025-05-22 19:25:52 +0300

    Merge pull request #1
    * feat: add App class with Hilt and Timber setup

---
+++ app/src/main/java/com/gwolf/nytbestsellers/App.kt
@@ -0,0 +1,18 @@
+package com.gwolf.nytbestsellers
+
+import android.app.Application
+import com.google.firebase.BuildConfig
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
