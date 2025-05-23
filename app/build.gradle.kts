717a39f21c46432fffe34aa9a1abee981bc1d5ea
Artem Naumov <artem.naumov.dev@gmail.com>
2025-05-23 17:36:59 +0300

    chore: update version to v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "0.4.0"
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
