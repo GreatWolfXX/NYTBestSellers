a0fd2b3ef179cc9e322328b8fbe249709921a2f6
Artem Naumov <artem.naumov.dev@gmail.com>
2025-05-23 22:50:54 +0300

    fix: replaced logic in AuthWithGoogleUseCase using FirebaseRepository

--- app/src/main/java/com/gwolf/nytbestsellers/domain/usecase/AuthWithGoogleUseCase.kt
+++ app/src/main/java/com/gwolf/nytbestsellers/domain/usecase/AuthWithGoogleUseCase.kt
@@ -1,27 +1,18 @@
 package com.gwolf.nytbestsellers.domain.usecase
 
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.gwolf.nytbestsellers.domain.repository.FirebaseRepository
 import com.gwolf.nytbestsellers.util.AppError
 import com.gwolf.nytbestsellers.util.DataResult
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.flow.catch
 import javax.inject.Inject
 
 class AuthWithGoogleUseCase @Inject constructor(
-    private val auth: FirebaseAuth
+    private val firebaseRepository: FirebaseRepository
 ) {
-    operator fun invoke(idToken: String): Flow<DataResult<FirebaseUser?>> = flow {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        try {
-            auth.signInWithCredential(credential).await()
-
-            val user = auth.currentUser
-            emit(DataResult.Success(user))
-        } catch (e: Exception) {
-            emit(DataResult.Error(AppError.Unexpected(e)))
+    operator fun invoke(): Flow<DataResult<FirebaseUser?>> =
+        firebaseRepository.signInWithGoogle().catch { exception ->
+            emit(DataResult.Error(AppError.Unexpected(exception)))
         }
-    }
 }
