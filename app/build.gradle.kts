7470f7e3d36892ef7435e413382d281b0b49c627
Artem Naumov <artem.naumov.dev@gmail.com>
2025-05-24 22:23:25 +0300

    feat: add CoreLibraryDesugar to support working with datetime on older versions of android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
         }
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -73,6 +74,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Core Library Desugar
+    coreLibraryDesugaring(libs.desugar.library)
 
     // Navigation
     implementation(libs.navigation.compose)
